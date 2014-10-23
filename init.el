5f429e3eb336dddaa7d3318240d06f47d935f58b
ice <meltedise@gmail.com>
2014-10-23 22:42:00 +0900

    add basic custom keybindings.

--- init.el
+++ init.el
@@ -1,7 +1,26 @@
 ;; -*- coding: utf-8 -*-
 
-;; Emacs package system
+
+;;; Keybindings
+(keyboard-translate ?\C-h ?\C-?) ; (global-set-key "\C-h" 'delete-backward-char)
+(global-set-key "\C-ch" 'help-command)
+;; (global-set-key "\C-xh" 'mark-whole-buffer)
+(global-set-key "\C-xg" 'goto-line)
+(global-set-key "\C-xl" 'linum-mode)
+(global-set-key "\C-c+" 'text-scale-increase)
+(global-set-key "\C-c-" 'text-scale-decrease)
+(global-set-key "\M-o"  'other-window) ;; 'other-window-or-split
+(global-set-key "\C-x\C-d" 'delete-region)
+
+
+;;; Emacs package system
 ;; M-x package-list-packages
+;; 【Enter ↵】 Describe the package under cursor. (describe-package)
+;; 【i】 mark for installation. (package-menu-mark-install)
+;; 【u】 unmark. (package-menu-mark-unmark)
+;; 【d】 mark for deletion (removal of a installed package). (package-menu-mark-delete)
+;; 【x】 for “execute” (start install/uninstall of marked items). (package-menu-execute)
+;; 【r】 refresh the list from server. (package-menu-refresh)
 (require 'package)
 (setq package-user-dir
       (concat (file-name-directory load-file-name) "elpa"))
@@ -9,3 +28,12 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 ;;(require 'melpa)
+
+;; Auto install elisp packages
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (package-install package)))
+ '(
+   ;; Installed packages here
+   ))
