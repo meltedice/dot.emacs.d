c583a1340691c05cb1112203d7114a3b1dff92f0
ice <meltedise@gmail.com>
2014-10-23 21:49:43 +0900

    change elpa/ directory path to relative to calculate from init.el path.

--- init.el
+++ init.el
@@ -1,8 +1,11 @@
 ;; -*- coding: utf-8 -*-
 
 ;; Emacs package system
+;; M-x package-list-packages
 (require 'package)
+(setq package-user-dir
+      (concat (file-name-directory load-file-name) "elpa"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
-(require 'melpa)
+;;(require 'melpa)
