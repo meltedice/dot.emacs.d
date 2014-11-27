7d2b1b7ba3fb24ae039d7b72f048493316f509ee
ice <meltedise@gmail.com>
2014-11-27 15:20:47 +0900

    add kill-region-or-backward-kill-word.

--- init.el
+++ init.el
@@ -65,6 +65,17 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 ;;; C-k at beginning of line, kill whole line including "\n"
 (setq kill-whole-line t)
 
+;;; mark が設定されていないときは単語削除、それ以外は kill-region
+;; http://d.akinori.org/?date=20070703
+(defun kill-region-or-backward-kill-word (&optional arg)
+  "Kill a region or a word backward."
+  (interactive)
+  (if mark-active
+      (kill-region (mark) (point))
+    (backward-kill-word (or arg 1))))
+(define-key minibuffer-local-completion-map "\C-w" 'kill-region-or-backward-kill-word)
+(global-set-key "\C-w" 'kill-region-or-backward-kill-word)
+
 ;;; Keybindings
 (keyboard-translate ?\C-h ?\C-?) ; (global-set-key "\C-h" 'delete-backward-char)
 (global-set-key "\C-ch" 'help-command)
