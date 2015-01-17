4c4d414bb3b705d23c4920fa218791b24ba38983
ice <ice@npc162.local>
2015-01-17 16:36:55 +0900

    add coffee-mode settings

--- inits/80-modes.el
+++ inits/80-modes.el
@@ -1,5 +1,6 @@
 ;;; modes
 
+;;; Cask
 (add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
 (setq auto-mode-alist (cons '("\\.\\(text\\|md\\|mdt\\)\\'" . markdown-mode) auto-mode-alist))
 
@@ -9,3 +10,12 @@
 ;; http://www.textism.com/tools/textile/
 ;; http://hobix.com/textile/
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
+;;; Coffee mode
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
