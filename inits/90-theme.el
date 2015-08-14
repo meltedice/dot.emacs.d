1bbfe2c1e3992d495a1d8215a025656ba3cff787
ice <meltedise@gmail.com>
2015-08-15 02:57:46 +0900

    add configuration to apply theme to new frame

--- inits/90-theme.el
+++ inits/90-theme.el
@@ -7,3 +7,15 @@
 (load-theme 'matrix t)
 (require 'matrix-on-ice-theme)
 (load-theme 'matrix-on-ice t)
+
+;;; apply theme to new frame (like: C-x 5 b)
+;; http://stackoverflow.com/questions/3035919/cant-apply-color-theme-to-one-frame-in-emacs
+(defun apply-color-theme (frame)
+  "Apply color theme to a frame based on whether its a 'real'
+   window or a console window."
+  (select-frame frame)
+  (load-theme 'matrix t)
+  (load-theme 'matrix-on-ice t)
+  )
+
+(add-hook 'after-make-frame-functions 'apply-color-theme)
