e85f25369beaa014c4e250edbe161c1372c0fa06
ice <meltedise@gmail.com>
2017-01-05 13:27:51 +0900

    change sh-mode indent 4 to 2

--- inits/80-major-modes.el
+++ inits/80-major-modes.el
@@ -10,3 +10,10 @@
 ;; http://www.textism.com/tools/textile/
 ;; http://hobix.com/textile/
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
+
+;;; Shell-script mode
+;; Ref: https://keramida.wordpress.com/2008/08/08/tweaking-shell-script-indentation-in-gnu-emacs/
+(defun sh-mode-custom ()
+  (setq sh-basic-offset 2
+        sh-indentation 2))
+(add-hook 'sh-mode-hook 'sh-mode-custom)
