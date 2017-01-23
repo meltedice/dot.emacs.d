7e8e99a2781f4ae7d501a93dc21b91632ea2a85f
ice <meltedise@gmail.com>
2017-01-23 15:27:03 +0900

    customize ediff face

--- init.el
+++ init.el
@@ -164,4 +164,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ediff-current-diff-B ((t (:background "DarkOrchid4"))))
+ '(ediff-current-diff-C ((t (:background "DarkOrchid4"))))
+ '(ediff-even-diff-B ((t (:background "dark blue"))))
+ '(ediff-even-diff-C ((t (:background "dark blue"))))
+ '(ediff-odd-diff-B ((t (:background "dark blue"))))
+ '(ediff-odd-diff-C ((t (:background "dark blue")))))
