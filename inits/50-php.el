278fb2a33c8922bc986da9cf9f86b11196647808
ice <meltedise@gmail.com>
2017-04-10 18:03:40 +0900

    add php-mode

--- inits/50-php.el
+++ inits/50-php.el
@@ -1,5 +1,11 @@
 ;;; ruby
 
+;;; php-mode
+;; cscope is required for auto-complete
+;;
+;; Windows 10
+;;   mingw64 % pacman -S cscope
+
 (add-hook 'php-mode-hook
           (lambda ()
             ;; TODO
