b9e9fb33f88f657e286a517c69965de416d81301
ice <meltedise@gmail.com>
2017-04-10 15:35:47 +0900

    testing font-sets

--- inits/windows-font.el
+++ inits/windows-font.el
@@ -1,15 +1,41 @@
 ;;; Font configuration for Windows 10
 ;; http://xiuxing.blog.jp/archives/8346924.html
 
-(set-default-font "MeiryoKe_Console 10")
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("MeiryoKe_Console" . "unicode-bmp")
-                  ;; '("Ricty Diminished" . "unicode-bmp")
-                  )
+;;; OK
+;; (set-default-font "MeiryoKe_Console 10")
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0208
+;;                   '("MeiryoKe_Console" . "unicode-bmp")
+;;                   ;; '("Ricty Diminished" . "unicode-bmp")
+;;                   )
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'katakana-jisx0201
+;;                   '("MeiryoKe_Console" . "unicode-bmp")
+;;                   ;; '("Ricty Diminished" . "unicode-bmp")
+;;                   )
 
-(set-fontset-font (frame-parameter nil 'font)
-                  'katakana-jisx0201
-                  '("MeiryoKe_Console" . "unicode-bmp")
-                  ;; '("Ricty Diminished" . "unicode-bmp")
-                  )
+;;; NG
+;;; http://qiita.com/melito/items/238bdf72237290bc6e42
+;; 全角と半角で横幅が合わなくてズレる...
+;; (set-face-attribute 'default nil :family "Inconsolata" :height 110)
+;; ;; (set-face-attribute 'default nil :family "Consolas" :height 104)
+;; (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "MeiryoKe_Console"))
+;; (setq face-font-rescale-alist '(("MeiryoKe_Console" . 1.08)))
+
+;;; NG
+;; 全角と半角で横幅が合わなくてズレる...
+;; (set-face-attribute 'default nil :family "MeiryoKe_Console")
+;; (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "MeiryoKe_Console"))
+;; (setq face-font-rescale-alist '(("MeiryoKe_Console" . 1.08)))
+
+;;; OK
+;; (set-default-font "MeiryoKe_Console 10")
+;; (set-fontset-font nil 'japanese-jisx0208 '("MeiryoKe_Console" . "unicode-bmp"))
+;; (set-fontset-font nil 'katakana-jisx0201'("MeiryoKe_Console" . "unicode-bmp"))
+
+;;; OK
+(set-default-font "Ricty Diminished 14")
+(set-fontset-font nil 'japanese-jisx0208 '("Ricty Diminished" . "unicode-bmp"))
+(set-fontset-font nil 'katakana-jisx0201 '("Ricty Diminished" . "unicode-bmp"))
+
+;; (set-face-font 'default "Ricty Diminished-15")
