5909438dfc39514668d3b153ca583cff0d02e429
ice <meltedise@gmail.com>
2017-04-26 10:02:00 +0900

    improve encoding and mode config around php mode

--- inits/50-web.el
+++ inits/50-web.el
@@ -2,19 +2,26 @@
 
 ;; See: http://umi-uyura.hatenablog.com/entry/2015/05/13/214629
 
+(add-to-list 'auto-mode-alist '("\\.ctp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
 ;; (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.js\\'"  . web-mode))
 
-;; (defun web-mode-custom ()
-;;   "Customize Web mode."
-;;   (setq web-mode-attr-indent-offset nil)
-;;   (setq web-mode-markup-indent-offset 2)
-;;   (setq web-mode-css-indent-offset 2)
-;;   (setq web-mode-code-indent-offset 2)
-;;   (setq web-mode-sql-indent-offset 2)
-;;   (setq indent-tabs-mode nil)
-;;   (setq tab-width 2))
-;; (add-hook 'web-mode-hook 'web-mode-custom)
+(defun web-mode-custom ()
+  "Customize Web mode."
+  ;; (setq web-mode-attr-indent-offset nil)
+  ;; (setq web-mode-markup-indent-offset 2)
+  ;; (setq web-mode-css-indent-offset 2)
+  ;; (setq web-mode-code-indent-offset 2)
+  ;; (setq web-mode-sql-indent-offset 2)
+  ;; (setq indent-tabs-mode nil)
+  ;; (setq tab-width 2)
+  (setq web-mode-engines-alist
+        '(("php" . "\\.ctp\\'"))
+        )
+  )
+(add-hook 'web-mode-hook 'web-mode-custom)
 
 ;; ;; npm install -g jsxhint
 ;; (require 'flycheck)
--- inits/80-encodings.el
+++ inits/80-encodings.el
@@ -3,10 +3,14 @@
 ;; Regexp: http://flex.ee.uec.ac.jp/texi/emacs-jp/emacs-jp_53.html
 ;; \' は空の文字列にマッチしますが，バッファの最後にあるものにだけです．
 ;; shift_jis-dos / utf-8-unix / utf-8 / euc-jp
-(modify-coding-system-alist 'file "\\.html\\'" 'utf-8-unix)
+;;(modify-coding-system-alist 'file "\\.html\\'" 'utf-8-unix)
 ;;(modify-coding-system-alist 'file "\\.htm\\'"  'shift_jis)
 ;;(modify-coding-system-alist 'file "\\.\\(pl\\|cgi\\|pm\\|t\\)\\(\\.~BASE~\\|:magit-ediff\\*\\|\\)\\'" 'utf-8-unix)
-(modify-coding-system-alist 'file "\\.\\(php\\|mdl\\|inc\\)\\'" 'utf-8-unix)
+(modify-coding-system-alist 'file "\\.\\(php\\|mdl\\|inc\\)\\'" 'utf-8) ;; 'utf-8-unix
 (modify-coding-system-alist 'file "\\.\\(rb\\|erb\\|yml\\)\\(\\.~BASE~\\|\\)\\'" 'utf-8-unix)
 (modify-coding-system-alist 'file "\\ChangeLog\\'" 'utf-8-unix)
-(modify-coding-system-alist 'file "\\.\\(txt\\|log\\|org\\)\\'" 'utf-8-unix)
+(modify-coding-system-alist 'file "\\.\\(md\\)\\'" 'utf-8-unix)
+(modify-coding-system-alist 'file "\\.\\(org\\)\\'" 'utf-8-unix)
+(modify-coding-system-alist 'file "\\.\\(txt\\|log\\)\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.ctp\\'" 'utf-8)
+(modify-coding-system-alist 'file "\\.html?\\'" 'utf-8)
