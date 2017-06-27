bc97e12b687ef46d059a6b447cf57f3eb20178d1
ice <meltedise@gmail.com>
2017-06-27 17:41:58 +0900

    add org-mode settings

---
+++ inits/50-org-mode.el
@@ -0,0 +1,8 @@
+;;; org-mode
+
+;; (setq org-todo-keywords
+;;       '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "SOMEDAY(s)")))
+;; (setq org-todo-keywords
+;;       '((sequence "TODO" "InProgress" "|" "DONE")))
+
+(setq org-log-done 'time)
