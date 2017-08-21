e35014091a46e2a38502c9155e5ef63f37058200
ice <meltedise@gmail.com>
2017-08-21 09:34:33 +0900

    set default encoding to utf-8-unix

--- inits/80-encodings.el
+++ inits/80-encodings.el
@@ -1,8 +1,13 @@
-;;; encodings
+;;; encodings --- encodings settings
 
 ;; Regexp: http://flex.ee.uec.ac.jp/texi/emacs-jp/emacs-jp_53.html
 ;; \' は空の文字列にマッチしますが，バッファの最後にあるものにだけです．
 ;; shift_jis-dos / utf-8-unix / utf-8 / euc-jp
+;;; Commentary:
+
+;;; Code:
+(modify-coding-system-alist 'file ".*\\'" 'utf-8-unix) ;; default
+
 ;;(modify-coding-system-alist 'file "\\.html\\'" 'utf-8-unix)
 ;;(modify-coding-system-alist 'file "\\.htm\\'"  'shift_jis)
 ;;(modify-coding-system-alist 'file "\\.\\(pl\\|cgi\\|pm\\|t\\)\\(\\.~BASE~\\|:magit-ediff\\*\\|\\)\\'" 'utf-8-unix)
@@ -16,3 +21,6 @@
 (modify-coding-system-alist 'file "\\.\\(html?\\|slim\\|haml\\)\\(\\.~BASE~\\|\\)\\'" 'utf-8)
 (modify-coding-system-alist 'file "\\.\\(css?\\|sass\\|scss\\|less\\)\\(\\.~BASE~\\|\\)\\'" 'utf-8)
 (modify-coding-system-alist 'file "\\.\\(js\\|jsx\\)\\(\\.~BASE~\\|\\)\\'" 'utf-8-unix)
+(modify-coding-system-alist 'file "\\.\\(lock\\)\\(\\.~BASE~\\|\\)\\'" 'utf-8-unix)
+
+;;; 80-encodings.el ends here
