b09af01073edaabdab05a1aceecb97bf0e40680a
ice <meltedise@gmail.com>
2017-09-21 16:53:11 +0900

    fix flycheck configuration for es6

--- inits/20-flymake.el
+++ inits/20-flymake.el
@@ -8,8 +8,12 @@
 ;;  '(flymake-errline ((((class color)) (:underline "red"))))
 ;;  '(flymake-warnline ((((class color)) (:underline "yellow")))))
 
+(setq flymake-no-changes-timeout 5)
+(setq flymake-start-syntax-check-on-newline nil)
 
 (setq flycheck-checker-error-threshold 1000) ; default 400
+(setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)) ; default '(save idle-change new-line mode-enabled)
+(setq flycheck-idle-change-delay 5) ; default 0.5
 
 
 (add-hook 'flymake-mode-hook
