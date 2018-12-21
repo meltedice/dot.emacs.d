b4c587876fcc5602a63440193e6bbc1aa38b81cc
ice <meltedise@gmail.com>
2018-12-21 12:31:45 +0900

    treat .flow file as javascript file

--- inits/50-javascript.el
+++ inits/50-javascript.el
@@ -46,7 +46,7 @@
 ;;; rjsx-mode
 ;; https://github.com/felipeochoa/rjsx-mode
 ;; https://joppot.info/2017/04/07/3734
-(add-to-list 'auto-mode-alist '(".*\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '(".*\\.\\(js\\|flow\\)\\'" . rjsx-mode))
 ;; (add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 ;; (add-to-list 'auto-mode-alist '("containers\\/.*\\.js\\'" . rjsx-mode))
 (defun rjsx-mode-custom ()
