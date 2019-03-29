54986c72b1209a19e1e5855d4be4123637d7f59a
ice <meltedise@gmail.com>
2019-03-29 11:32:58 +0900

    add typescript configuration

--- inits/50-javascript.el
+++ inits/50-javascript.el
@@ -1,5 +1,4 @@
 ;;; javscript-related-modes --- javascript related modes settings
-
 ;; Author: ice <meltedise@gmail.com>
 ;; Created:
 ;; Version:
@@ -74,10 +73,9 @@
 ;; TypeScript: https://github.com/ananthakumaran/typescript.el
 ;; https://www.emacswiki.org/emacs/TypeScript
 (add-to-list 'auto-mode-alist '("\\.\\(ts\\|tsx\\)\\'" . typescript-mode))
-(add-hook 'typescript-mode-hook 'flycheck-mode)
 
 ;; TODO: tss.el
-;; TODO: tide
+
 
 ;;; flycheck
 (require 'flycheck)
@@ -91,6 +89,7 @@
 ;; (add-hook 'js2-mode-hook 'flycheck-mode)
 ;; (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
 (add-hook 'rjsx-mode-hook 'flycheck-mode)
+(add-hook 'typescript-mode-hook 'flycheck-mode)
 
 (require 'flycheck-flow)
 ;; (with-eval-after-load 'flycheck
@@ -119,6 +118,32 @@
 
 ;;; M-x flycheck-verify-setup
 
+
+;;; tide
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  ;; (company-mode +1)
+  )
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; disable tide's code format, leave this to prettier + eslint
+;; ;; formats the buffer before saving
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+;;; prettier
 (require 'prettier-js)
 ;; (setq prettier-js-command "eslint") ;; "prettier"
 ;; (setq prettier-js-args '("--fix")) ;; '()
